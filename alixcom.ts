545801c6f9af80d45832df40b63b637ff8f153b5
lessneek <1essn33k@gmail.com>
2020-02-22 14:17:59 +0300

    Added license.

--- alixcom.ts
+++ alixcom.ts
@@ -1,3 +1,19 @@
+//     Firefox add-on which replaces all wrong language links on the `aliexpress.com`.
+//     Copyright (C) 2020  Alexander Logger
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 console.log("alixcom: loaded.");
 
 function replaceLinks(links: HTMLCollectionOf<HTMLAnchorElement>) {
